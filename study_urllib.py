de18cda8b07afb557e9860e3169ffa055a9ba26b
longsimiao <469611602@qq.com>
2017-04-20 00:15:12 -0700

    update for study_urllib.py

--- study_urllib.py
+++ study_urllib.py
@@ -9,8 +9,37 @@ import csv
 random.seed(datetime.datetime.now())
 
 
+def get_url_lists(start_url, crawl_url_lists=[]):
+    """
+    Get url list.
+    :param start_url: Start url.
+    :param crawl_url_lists: A list to store being crawled url list.
+    :return: To be crawled url list.
+    """
+    # Determine whether start_url exits in crawl_url_lists or not.
+    if start_url not in crawl_url_lists:
+        crawl_url_lists.append(start_url)
+
+    req_content = urlopen(start_url)
+    bs_obj = BeautifulSoup(req_content.read(), 'html.parser')
+    next_page_url_obj = bs_obj.findAll("a", {"class": "pn-next"})
+    hostname = "https://list.jd.com"
+    next_page_url = hostname + next_page_url_obj[0]["href"]
+
+    if next_page_url not in crawl_url_lists:
+        crawl_url_lists.append(next_page_url)
+        # callback get_url_lists()
+        get_url_lists(next_page_url, crawl_url_lists)
+
+    return crawl_url_lists
+
+
 def get_phone_data_from_jd(start_url):
-    crawl_url_lists = []
+    """
+    爬取内容
+    :param start_url: Start url.
+    :return: Data to be stored.
+    """
     headers = {
                 'Accept': '*/*',
                 'Accept-Encoding': 'gzip, deflate, sdch, br',
@@ -26,8 +55,6 @@ def get_phone_data_from_jd(start_url):
 
     phone_lists = bs_obj.findAll("li", {"class": "gl-item"})
     # print(phone_lists[0])
-    # 已爬url池
-    crawl_url_lists.append(start_url)
     phone_row = []
     for phone_obj in phone_lists:
         temp_phone_row = []
@@ -48,7 +75,6 @@ def get_phone_data_from_jd(start_url):
         json_price = requests.get(json_price_url, headers).text
         price_data = json.loads(json_price)[0]
         phone_price = price_data["op"]
-        print(phone_sku)
 
         comments_url = 'https://club.jd.com/comment/productCommentSummaries.action?referenceIds=' \
                        + phone_sku
@@ -79,26 +105,56 @@ def get_phone_data_from_jd(start_url):
         temp_phone_row.append(good_comment_rate)
         temp_phone_row.append(poor_comment_rate)
         temp_phone_row.append(general_comment_rate)
-        print(temp_phone_row)
+
         phone_row.append(temp_phone_row)
 
-    # 下一页URL
-    next_page_url_obj = bs_obj.findAll("a", {"class": "pn-next"})
-    hostname = "https://list.jd.com"
-    next_page_url = hostname + next_page_url_obj[0]["href"]
+    return phone_row
 
-    if next_page_url not in crawl_url_lists:
-        crawl_url_lists.append(next_page_url)
-        get_phone_data_from_jd(next_page_url)
-    return phone_row, crawl_url_lists
 
-start_url = "https://list.jd.com/list.html?cat=9987,653,655"
-csv_data = get_phone_data_from_jd(start_url)
+def main_control(start_url):
+    """
+    每一页存为一个CSV文件
+    :param start_url: Start url.
+    :return: None.
+    """
+    crawl_url_line = get_url_lists(start_url)
+    i = 0
 
-with open("test.csv", "w") as f:
+    for url in crawl_url_line:
+        csv_data = get_phone_data_from_jd(url)
+        i += 1
+        csv_file_name = "res" + str(i)
+        with open(csv_file_name + ".csv", "w") as f:
+            header = ["店铺", "简介", "SKU", "价格", "总评", "好评", "追评", "中评", "差评",
+                       "好评率", "差评率", "中评率"]
+            f_csv = csv.writer(f)
+            f_csv.writerow(header)
+            f_csv.writerows(csv_data)
+
+
+def main_control_res(start_url):
+    """
+    所有结果存为一个CSV文件
+    :param start_url: Start url.
+    :return: None.
+    """
+    crawl_url_line = get_url_lists(start_url)
+    res_csv = []
+    for url in crawl_url_line:
+        csv_data = get_phone_data_from_jd(url)
+        res_csv += csv_data
+    return res_csv
+
+
+begin_url = "https://list.jd.com/list.html?cat=9987,653,655"
+
+csv_data_res = main_control_res(begin_url)
+csv_name = "res"
+with open(csv_name + ".csv", "w") as f:
     headers = ["店铺", "简介", "SKU", "价格", "总评", "好评", "追评", "中评", "差评",
                "好评率", "差评率", "中评率"]
     f_csv = csv.writer(f)
     f_csv.writerow(headers)
-    f_csv.writerows(csv_data)
+    f_csv.writerows(csv_data_res)
 
+main_control_res(begin_url)
