74c1eb59d8d8691f870f1bde815d8fb44a49eaf5
longsimiao <469611602@qq.com>
2017-04-24 21:18:12 -0700

    update 2017.4.25 12:18

--- storeDataToMysql.py
+++ storeDataToMysql.py
@@ -1,174 +1,129 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-from urllib.request import urlopen, build_opener
-from bs4 import BeautifulSoup
-import random
-import datetime
-import requests
-import json
-import csv
-
-
-random.seed(datetime.datetime.now())
+# @Time    : 4/21/17 12:25 AM
+# @Author  : Ferras
+# @Email   : mxlmiao@sina.cn
+# @File    : test_next_url.py
+# @Software: PyCharm
+import re
+import pymysql
+from MyGit.crawlDataFromJD import get_phone_data_from_jd
 
 
-def get_url_lists(start_url, crawl_url_lists=[]):
+def judge_table_exist(table_name, database_name):
     """
-    获取待爬取 url 列表
-    :param start_url: Start url.
-    :param crawl_url_lists: A list to store being crawled url list.
-    :return: To be crawled url list.
+    判断表是否已存在
+    :param table_name:
+    :param database_name:
+    :return:
     """
-    # 判断url是否已在待爬取池中
-    if start_url not in crawl_url_lists:
-        crawl_url_lists.append(start_url)
+    conn = pymysql.connect(host='localhost', user='root',
+                           passwd='root', db=database_name,
+                           charset='utf8')
+    cur = conn.cursor()
+    try:
+        show_table_sql = "SHOW TABLES;"
+        cur.execute(show_table_sql)
+        tables = [cur.fetchall()]
+        table_list = re.findall('(\'.*?\')', str(tables))
+        table_list = [re.sub("'", '', each) for each in table_list]
+        if table_name in table_list:
+            print('Table', table_name, 'already exists')
+            return 1
+        else:
+            print('Table', table_name, 'not exists')
+            return 0
+    except Exception as e:
+        print("<--出现异常-->" + str(e))
+    finally:
+        cur.close()
+        conn.close()
 
-    headers = ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
-                             'AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/55.0.2883.9 Safari/537.36')
-    build_opener().addheaders = [headers]
-    req_content = build_opener().open(start_url)
-    bs_obj = BeautifulSoup(req_content.read(), 'html.parser')
-    next_page_url_obj = bs_obj.findAll("a", {"class": "pn-next"})
-    hostname = "https://list.jd.com"
-    # '#J_main'需保留，否则无法抓取全部url
-    next_page_url = hostname + next_page_url_obj[0]["href"] + '#J_main'
 
-    if next_page_url not in crawl_url_lists:
-        crawl_url_lists.append(next_page_url)
-        # callback get_url_lists()
-        get_url_lists(next_page_url, crawl_url_lists)
-
-    return crawl_url_lists
-
-
-def get_phone_data_from_jd(start_url):
+def create_table(table_name, database_name):
     """
-    爬取内容
-    :param start_url: Start url.
-    :return: Data to be stored.
+    输入要创建的表名和要使用的数据库名称
+    :param table_name: 要创建的表名称
+    :param database_name: 所使用的数据库名称
+    :return:
     """
-    headers = ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) '
-                             'AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/55.0.2883.9 Safari/537.36')
-    build_opener().addheaders = [headers]
-    req_content = build_opener().open(start_url)
-    bs_obj = BeautifulSoup(req_content.read(), 'html.parser')
+    conn = pymysql.connect(host='localhost', user='root',
+                           passwd='root', db=database_name,
+                           charset='utf8')
+    cur = conn.cursor()
+    try:
 
-    phone_lists = bs_obj.findAll("li", {"class": "gl-item"})
-    # print(phone_lists[0])
-    phone_row = []
-    for phone_obj in phone_lists:
-        temp_phone_row = []
-        # 店铺名称 store_name
-        store_obj = phone_obj.findAll("div", {"class": "p-shop"})
-        store_name = store_obj[0]["data-shop_name"]
+        create_table_sql = """
+            CREATE TABLE %s(
+            id BIGINT(7) NOT NULL AUTO_INCREMENT,
+            store VARCHAR(200),
+            brief VARCHAR(200),
+            SKU VARCHAR(200),
+            price VARCHAR(200),
+            total_comment VARCHAR(200),
+            good_comment VARCHAR(200),
+            after_comment VARCHAR(200),
+            general_comment VARCHAR(200),
+            poor_comment VARCHAR(200),
+            good_comment_rate VARCHAR(200),
+            poor_comment_rate VARCHAR(200),
+            general_comment_rate VARCHAR(200),
+            created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY(id)
+            )CHARACTER SET=utf8;
+            """ % table_name
 
-        # 手机简介
-        phone_brief_obj = phone_obj.findAll("em")
-        phone_brief = phone_brief_obj[-1].string
-
-        # SKU
-        phone_sku_obj = phone_obj.findAll("div", {"class": "j-sku-item"})
-        phone_sku = phone_sku_obj[0]["data-sku"]
-
-        # 价格 phone_price
-        # json_price_url = 'https://p.3.cn/prices/mgets?skuIds=J_' + phone_sku
-        guess_num_1 = random.randrange(10000000, 99999999)
-        json_price_url = 'https://p.3.cn/prices/mgets?pduid=14926716573134' \
-                         + str(guess_num_1) + '&skuIds=J_' + phone_sku
-        json_price = requests.get(json_price_url, headers).text
-        price_data = json.loads(json_price)[0]
-        phone_price = price_data["op"]
-
-        # comments_url = 'https://club.jd.com/comment/product' \
-        #                'CommentSummaries.action?referenceIds=' + phone_sku
-        guess_num_2 = random.randrange(10000000, 99999999)
-        comments_url = 'https://club.jd.com/comment/productComment' \
-                       'Summaries.action?referenceIds=' + phone_sku \
-                       + '&_=14927' + str(guess_num_2)
-        # get json comments data
-        comments_json_data = requests.get(comments_url, headers).text
-        # Get comments dict. CommentsCount for key and a list for value.
-        comments_data = json.loads(comments_json_data)
-        comments_list = comments_data.get("CommentsCount")[0]
-
-        comment_count = comments_list.get("CommentCountStr")
-        good_comment_count = comments_list.get("GoodCountStr")
-        after_comment_count = comments_list.get("AfterCountStr")
-        general_comment_count = comments_list.get("GeneralCountStr")
-        poor_comment_count = comments_list.get("PoorCountStr")
-        good_comment_rate = comments_list.get("GoodRate")
-        poor_comment_rate = comments_list.get("PoorRate")
-        general_comment_rate = comments_list.get("GeneralRate")
-
-        temp_phone_row.append(store_name)
-        temp_phone_row.append(phone_brief)
-        temp_phone_row.append(phone_sku)
-        temp_phone_row.append(phone_price)
-        temp_phone_row.append(comment_count)
-        temp_phone_row.append(good_comment_count)
-        temp_phone_row.append(after_comment_count)
-        temp_phone_row.append(general_comment_count)
-        temp_phone_row.append(poor_comment_count)
-        temp_phone_row.append(good_comment_rate)
-        temp_phone_row.append(poor_comment_rate)
-        temp_phone_row.append(general_comment_rate)
-
-        print(temp_phone_row)
-
-        phone_row.append(temp_phone_row)
-
-    return phone_row
+        cur.execute(create_table_sql)
+    except Exception as e:
+        print("<--出现异常-->" + str(e))
+    finally:
+        cur.close()
+        conn.close()
 
 
-def main_control(start_url):
+def store_data(table_name, database_name, start_url):
     """
-    每一页存为一个CSV文件
-    :param start_url: Start url.
-    :return: None.
+    将爬取到的数据存储到mysql中
+    :param table_name: 使用的表名称
+    :param database_name: 使用的数据库名称
+    :param start_url: 起始url
+    :return:
     """
-    crawl_url_line = get_url_lists(start_url)
-    i = 0
+    # 判断数据要存储的表是否存在
+    if judge_table_exist(table_name, database_name) == 0:
+        create_table(table_name, database_name)
 
-    for url in crawl_url_line:
-        csv_data = get_phone_data_from_jd(url)
-        i += 1
-        csv_file_name = "res" + str(i)
-        with open(csv_file_name + ".csv", "w") as f:
-            header = ["店铺", "简介", "SKU", "价格", "总评", "好评",
-                      "追评", "中评", "差评", "好评率", "差评率", "中评率"]
-            f_csv = csv.writer(f)
-            f_csv.writerow(header)
-            f_csv.writerows(csv_data)
+    # 连接数据库，准备存储数据
+    conn = pymysql.connect(host='localhost', user='root',
+                           passwd='root', db=database_name, charset='utf8')
+    cur = conn.cursor()
+    try:
+
+        # get_phone_data_from_jd()获取一页数据
+        sql_data = get_phone_data_from_jd(start_url)
+        print(len(sql_data))
+        for data in sql_data:
+            # 存储爬取数据
+            store_data_sql = u"""
+            INSERT INTO %s (store, brief, SKU, price, total_comment,
+            good_comment, after_comment, general_comment, poor_comment,
+            good_comment_rate, poor_comment_rate, general_comment_rate)
+            VALUES(\"%s", \"%s", \"%s", \"%s" ,\"%s", \"%s",
+            \"%s", \"%s", \"%s", \"%s", \"%s", \"%s")
+            """ % (table_name, data[0], data[1], data[2],
+                   data[3], data[4], data[5], data[6],
+                   data[7], data[8], data[9], data[10],
+                   data[11])
+
+            cur.execute(store_data_sql)
+            cur.connection.commit()
+    except Exception as e:
+        print("<--出现异常-->" + str(e))
+    finally:
+        cur.close()
+        conn.close()
+
+begin_url = "https://list.jd.com/list.html?cat=9987,653,655"
+store_data("jd_dat", "mytest", begin_url)
 
 
-def main_control_res(start_url):
-    """
-    所有结果存为一个CSV文件
-    :param start_url: Start url.
-    :return: None.
-    """
-    crawl_url_line = get_url_lists(start_url)
-    res_csv = []
-    for url in crawl_url_line:
-        csv_data = get_phone_data_from_jd(url)
-        res_csv += csv_data
-    return res_csv
-
-
-# begin_url = "https://list.jd.com/list.html?cat=9987,653,655"
-#
-# csv_data_res = main_control_res(begin_url)
-# csv_name = "res"
-# with open(csv_name + ".csv", "w") as f:
-#     headers = ["店铺", "简介", "SKU", "价格", "总评", "好评", "追评",
-#                "中评", "差评","好评率", "差评率", "中评率"]
-#     f_csv = csv.writer(f)
-#     f_csv.writerow(headers)
-#     f_csv.writerows(csv_data_res)
-#
-# main_control_res(begin_url)
-
-# get_phone_data_from_jd(begin_url)
